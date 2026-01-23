77a3a74d7701e8a130734c78ab1820e0c076bf85
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-23 09:19:04 +0200

    commit

--- first.php
+++ first.php
@@ -5,12 +5,6 @@ for ($j=0; $j<5; $j++){
     }
      echo "<br>";
 }
-for ($j=0; $j<5; $j++){
-    for($i=0; $i<$j; $i++){
-        echo "* ";
-    }
-     echo "<br>";
-}
 for($j=1; $j<5; $j++){
     for($i=5; $i>$j; $i--){
         echo "* ";
