fe933fc1d849d3619135ac958c17ebd6c2a0b298
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-02-06 11:45:00 +0200

    commit

--- filefunctions.php
+++ filefunctions.php
@@ -1,5 +1,11 @@
 <?php
-$file = fopen("renamed.txt", "r");
-$newfile = fgets($file);
-echo $newfile;
-fclose($file);
+// $file = fopen("renamed.txt", "r");
+// $newfile = fgets($file);
+// echo $newfile;
+// fclose($file);
+
+copy('renamed.txt', 'new.txt');
+$newrenamed = file_get_contents('new.txt');
+echo $newrenamed;
+
+unlink('new.txt');
